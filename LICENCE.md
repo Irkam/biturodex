b3855da5d3d3d650a21337ffea35fab9dff748f7
Jivay <hay.jeanvincent@orbital-chainsaw.net>
2014-02-23 18:34:50 +0100

    Modification du README, ajout des PDF et ajout de la base Django

---
+++ LICENCE.md
@@ -0,0 +1,16 @@
+The DO WHAT THE FUCK YOU WANT BeerWare LICENSE
+                    Version 42, December 2013
+
+ Copyright (C) 2013 ACME
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+ 
+   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+   1. You just DO WHAT THE FUCK YOU WANT TO.
+   2. If we meet some day, and you think this stuff is worth it, you can buy us a beer in return.
+   3. if you think this stuff is useless, you can buy us a beer anyway. 
+   4. if someone sold you this software, you've been f*ck*d up.
---
+++ db/db.sql
@@ -0,0 +1,237 @@
+-- Parse::SQL::Dia          version 0.27                              
+-- Documentation            http://search.cpan.org/dist/Parse-Dia-SQL/
+-- Environment              Perl 5.018001, /usr/bin/perl              
+-- Architecture             x86_64-linux-gnu-thread-multi             
+-- Target Database          mysql-innodb                              
+-- Input file               db.dia                                    
+-- Generated at             Wed Feb 12 18:26:00 2014                  
+-- Typemap for mysql-innodb not found in input file                   
+
+-- get_constraints_drop 
+alter table user_stats drop foreign key user_stats_fk_Uid ;
+alter table employees drop foreign key employees_fk_Uid ;
+alter table employees drop foreign key employees_fk_Id_establishment ;
+alter table establishment drop foreign key establishment_fk_Id_type ;
+alter table establishment_stats drop foreign key estblshmnt_sts_fk_Id_stblshmnt ;
+alter table event drop foreign key event_fk_Id_type ;
+alter table event drop foreign key event_fk_Id_establishment ;
+alter table friendlist drop foreign key friendlist_fk_Friend_uid ;
+alter table friend_invite drop foreign key friend_invite_fk_To_uid ;
+alter table event_invite drop foreign key event_invite_fk_To_uid ;
+alter table event_stats drop foreign key event_stats_fk_Id_event ;
+alter table event_participation drop foreign key evnt_prtcptn_fk_Id_event ;
+alter table event_participation drop foreign key event_participation_fk_Uid ;
+alter table message drop foreign key message_fk_From_uid ;
+alter table message drop foreign key message_fk_Id_conversation ;
+alter table conversation_subscribe drop foreign key cnvrstn_sbscrb_fk_Id_cnvrstn ;
+alter table conversation_subscribe drop foreign key conversation_subscribe_fk_Uid ;
+
+-- get_permissions_drop 
+
+-- get_view_drop
+
+-- get_schema_drop
+drop table if exists user;
+drop table if exists friendlist;
+drop table if exists establishment;
+drop table if exists employees;
+drop table if exists establishment_type;
+drop table if exists event;
+drop table if exists event_participation;
+drop table if exists user_stats;
+drop table if exists event_stats;
+drop table if exists establishment_stats;
+drop table if exists event_type;
+drop table if exists conversation;
+drop table if exists conversation_subscribe;
+drop table if exists message;
+drop table if exists event_invite;
+drop table if exists friend_invite;
+
+-- get_smallpackage_pre_sql 
+
+-- get_schema_create
+create table user (
+   uid       long         not null,
+   name      varchar(30)          ,
+   firstname varchar(30)          ,
+   username  varchar(60)          ,
+   passwd    varchar(256)         ,
+   sesstoken varchar(60)          ,
+   pushtoken varchar(60)          ,
+   latitude  double               ,
+   longitude double               ,
+   constraint pk_user primary key (uid)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table friendlist (
+   uid        long not null,
+   friend_uid long not null,
+   constraint pk_friendlist primary key (uid,friend_uid)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table establishment (
+   id_establishment long        not null,
+   id_type          int                 ,
+   name             varchar(60)         ,
+   address0         varchar(60)         ,
+   address2         varchar(60)         ,
+   city             varchar(30)         ,
+   postcode         varchar(6)          ,
+   latitude         double              ,
+   longitude        double              ,
+   constraint pk_establishment primary key (id_establishment)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table employees (
+   uid              long        not null,
+   id_establishment long        not null,
+   rights           int         not null,
+   label            varchar(60) not null,
+   constraint pk_employees primary key (uid,id_establishment)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table establishment_type (
+   id_type_establishment int         not null,
+   label_type            varchar(30)         ,
+   constraint pk_establishment_type primary key (id_type_establishment)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table event (
+   id_event         long         not null,
+   owner_uid        long         not null,
+   id_establishment long                 ,
+   latitude         double               ,
+   longitude        double               ,
+   range            int                  ,
+   begins           datetime             ,
+   ends             datetime             ,
+   id_type          int                  ,
+   address          varchar(250)         ,
+   constraint pk_event primary key (id_event)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table event_participation (
+   id_participation long    not null,
+   id_event         long            ,
+   uid              long            ,
+   yesnomaybe       tinyint         ,
+   confirmed        boolean         ,
+   constraint pk_event_participation primary key (id_participation)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table user_stats (
+   id_stat_user long         not null,
+   uid          long         not null,
+   from_uid     long         not null,
+   rating       tinyint      not null,
+   comment      varchar(250) not null,
+   constraint pk_user_stats primary key (id_stat_user)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table event_stats (
+   id_stat_event long         not null,
+   id_event      long                 ,
+   from_uid      long                 ,
+   rating        tinyint              ,
+   comment       varchar(150)         ,
+   constraint pk_event_stats primary key (id_stat_event)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table establishment_stats (
+   id_stat_establishment long         not null,
+   id_establishment      long                 ,
+   from_uid              long                 ,
+   rating                tinyint              ,
+   comment               varchar(150)         ,
+   constraint pk_establishment_stats primary key (id_stat_establishment)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table event_type (
+   id_type_event int         not null,
+   label_type    varchar(30)         ,
+   constraint pk_event_type primary key (id_type_event)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table conversation (
+   id_conversation long    not null,
+   closed          boolean         ,
+   constraint pk_conversation primary key (id_conversation)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table conversation_subscribe (
+   id_conversation long not null,
+   uid             long not null,
+   constraint pk_conversation_subscribe primary key (id_conversation,uid)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table message (
+   id_message      long not null,
+   id_conversation long         ,
+   from_uid        long         ,
+   message         text         ,
+   constraint pk_message primary key (id_message)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table event_invite (
+   id_event_invite long    not null,
+   from_uid        long            ,
+   to_uid          long            ,
+   event_id        long            ,
+   seen            boolean         ,
+   constraint pk_event_invite primary key (id_event_invite)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table friend_invite (
+   id_friend_invite long    not null,
+   from_uid         long            ,
+   to_uid           long            ,
+   seen             boolean         ,
+   constraint pk_friend_invite primary key (id_friend_invite)
+)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- get_view_create
+
+-- get_permissions_create
+
+-- get_inserts
+
+-- get_smallpackage_post_sql
+
+-- get_associations_create
+alter table user_stats add constraint user_stats_fk_Uid 
+    foreign key (uid)
+    references user (uid) ;
+alter table employees add constraint employees_fk_Uid 
+    foreign key (uid)
+    references user (uid) ;
+alter table employees add constraint employees_fk_Id_establishment 
+    foreign key (id_establishment)
+    references establishment (id_establishment) ;
+alter table establishment add constraint establishment_fk_Id_type 
+    foreign key (id_type)
+    references establishment_type (id_type_establishment) ;
+alter table establishment_stats add constraint estblshmnt_sts_fk_Id_stblshmnt 
+    foreign key (id_establishment)
+    references establishment (id_establishment) ;
+alter table event add constraint event_fk_Id_type 
+    foreign key (id_type)
+    references event_type (id_type_event) ;
+alter table event add constraint event_fk_Id_establishment 
+    foreign key (id_establishment)
+    references establishment (id_establishment) ;
+alter table friendlist add constraint friendlist_fk_Friend_uid 
+    foreign key (friend_uid)
+    references user (uid) ;
+alter table friend_invite add constraint friend_invite_fk_To_uid 
+    foreign key (to_uid)
+    references user (uid) ;
+alter table event_invite add constraint event_invite_fk_To_uid 
+    foreign key (to_uid)
+    references user (uid) ;
+alter table event_stats add constraint event_stats_fk_Id_event 
+    foreign key (id_event)
+    references event (id_event) ;
+alter table event_participation add constraint evnt_prtcptn_fk_Id_event 
+    foreign key (id_event)
+    references event (id_event) ;
+alter table event_participation add constraint event_participation_fk_Uid 
+    foreign key (uid)
+    references user (uid) ;
+alter table message add constraint message_fk_From_uid 
+    foreign key (from_uid)
+    references user (uid) ;
+alter table message add constraint message_fk_Id_conversation 
+    foreign key (id_conversation)
+    references conversation (id_conversation) ;
+alter table conversation_subscribe add constraint cnvrstn_sbscrb_fk_Id_cnvrstn 
+    foreign key (id_conversation)
+    references conversation (id_conversation) ;
+alter table conversation_subscribe add constraint conversation_subscribe_fk_Uid 
+    foreign key (uid)
+    references user (uid) ;

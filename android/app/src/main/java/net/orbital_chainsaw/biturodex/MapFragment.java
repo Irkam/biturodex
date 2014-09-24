3dc8f3a0aad5e8423db57bf83f1cdfd46d771f06
Jivay Hay <jivay@irkam-laptop>
2014-09-24 17:57:27 +0200

    gmap api ready

--- android/app/src/main/java/net/orbital_chainsaw/biturodex/MapFragment.java
+++ android/app/src/main/java/net/orbital_chainsaw/biturodex/MapFragment.java
@@ -1,7 +1,9 @@
 package net.orbital_chainsaw.biturodex;
 
+import android.app.Fragment;
+
 /**
- * Created by jivay on 17/09/14.
+ * Created by jivay on 24/09/14.
  */
-public class MapFragment {
+public class MapFragment extends Fragment {
 }
